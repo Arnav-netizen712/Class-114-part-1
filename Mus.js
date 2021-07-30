88a33a69784e48cad5d6664dd88f011d43aadb76
Arnav-netizen712 <87858916+Arnav-netizen712@users.noreply.github.com>
2021-07-30 19:40:01 +0530

    Add files via upload

---
+++ Mus.js
@@ -0,0 +1,18 @@
+    function preload(){
+    }   
+    
+    function setup(){
+        canvas = createCanvas(300,300);
+        canvas.center();                                                                                                                                                        
+    }
+    
+    function draw(){
+    }
+    
+    function Take_Snapshot(){
+        save('Mustache_Filter.png');
+    }
+
+    function Change_Screen1(){
+        window.location = "lip.html";
+    }
